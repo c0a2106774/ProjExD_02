99e11d1b525c2fa68123fa3760469c259ef21c9b
c0a2106774 <c0a2106774@edu.teu.ac.jp>
2023-11-28 16:44:40 +0900

    演習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,7 +13,6 @@ delat={
     pg.K_RIGHT:(+5,0)
 }
 
-
 def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
     """
     引数：こうかとんRect or 爆弾Rect
@@ -33,17 +32,28 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img2 = pg.image.load("ex02/fig/8.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_f = pg.transform.flip(kk_img,True,False)
+    kk_imgs = {(+5,0):kk_img_f,  # 右方向のこうかとん
+                (+5,-5):pg.transform.rotozoom(kk_img_f,45,1.0),  #右上方向のこうかとん
+               (0,-5):pg.transform.rotozoom(kk_img_f,90,1.0),  #上方向のこうかとん
+               (+5,+5):pg.transform.rotozoom(kk_img_f,-45,1.0),  #右下方向のこうかとん
+               (0,+5):pg.transform.rotozoom(kk_img_f,-90,1.0),  #下方向のこうかとん
+               (-5,+5):pg.transform.rotozoom(kk_img,45,1.0),  #左下方向のこうかとん
+               (-5,0):kk_img,  # 左方向のこうかとん
+               (-5,-5):pg.transform.rotozoom(kk_img,-45,1.0),  #左上方向のこうかとん
+                }
+    kk_img = kk_imgs[+5,0]
     kk_img2 = pg.transform.rotozoom(kk_img2, 0, 2.0)
-    bb_img = pg.Surface((20,20))
+    bb_img = pg.Surface((20, 20))   # 練習１：透明のSurfaceを作る
     bb_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    #accs = [a for a in range(1, 11)]
-    ##for r in range(1, 11):
-        ##bb_img = pg.Surface((20*r, 20*r))
-        ##pg.draw.circle(bb_img,(255,0,0),(10*r, 10*r), 10*r)
-        ##bb_imgs.append(bb_img)
-        
-    bb_img.set_colorkey((0,0,0)) #透過
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) 
+    #bb_imgs= []
+    #for r in range(1, 11):
+        #bb_img = pg.Surface((20*r, 20*r))
+        #pg.draw.circle(bb_img,(255,0,0),(10*r, 10*r), 10*r)
+        #bb_img.set_colorkey((0,0,0))
+        #bb_imgs.append(bb_img)
+    
     kk_rct= kk_img.get_rect()
     kk_rct.center = 900,400
     bb_rct = bb_img.get_rect()
@@ -74,16 +84,25 @@ def main():
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) != (True,True):
             kk_rct.move_ip(sum_mv[0], -sum_mv[1])
+
+        kk_0 = 0
+        kk_1 = 0
+        for k,mv in delat.items():
+            if key_lst[k]:
+                kk_0 = kk_0 + mv[0]
+                kk_1 = kk_1 + mv[1]
             
-        screen.blit(kk_img, kk_rct)  
-        #avx, avy = vx*accs[min(tmr//500,9)], vy*accs[min(tmr//500, 9)]
-        #bb_img = bb_imgs[min(tmr//500, 9)]      
+            if kk_0 != 0 or kk_1 !=0:
+                kk_img=kk_imgs[kk_0,kk_1]
+            
+        screen.blit(kk_img, kk_rct)       
         bb_rct.move_ip(vx,vy)
         yoko,tate= check_bound(bb_rct)
         if not yoko:
             vx *= -1
         if not tate:
             vy *= -1
+        #bb_img = bb_imgs[min(tmr//500, 9)] 
         screen.blit(bb_img,bb_rct)
         
         pg.display.update()
