e0ab5de478f2c390338d53c9b414504efcd8417f
c0a2106774 <c0a2106774@edu.teu.ac.jp>
2023-11-28 14:34:58 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,6 +6,13 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delat={
+    pg.K_UP:(0,-5),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(+5,0)
+}
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -16,20 +23,31 @@ def main():
     bb_img = pg.Surface((20,20)) #練習1:透明のSurfaceの円を作る
     pg.draw.circle(bb_img,(255,0,0),(10,10),10) #練習1: 赤い円を描く
     bb_img.set_colorkey((0,0,0)) #透過
+    kk_rct= kk_img.get_rect()
+    kk_rct.center = 900,400
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0,WIDTH)
     bb_rct.centery = random.randint(0,HEIGHT)
-        
+
+    vx = +5
+    vy = +5   
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+        key_lst = pg.key.get_pressed()
+        sum_mv= [0,0]
+        for k,tpl in delat.items():
+            if key_lst[k]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
         screen.blit(bb_img,bb_rct)
+        bb_rct.move_ip(vx,vy)
+        kk_rct.move_ip(sum_mv,sum_mv)
         pg.display.update()
         tmr += 1
         clock.tick(10)
